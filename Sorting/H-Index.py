d5d011a65d925cc3a672ac199b6d7f94283136ca
Abhijit <abhijeetrathore104@gmail.com>
2025-06-05 22:50:02 +0530

    Create H-Index.py

---
+++ Sorting/H-Index.py
@@ -0,0 +1,27 @@
+def hIndex(citations):
+    n = len(citations)
+    freq = [0] * (n + 1)
+
+    # Count the frequency of citations
+    for citation in citations:
+        if citation >= n:
+            freq[n] += 1
+        else:
+            freq[citation] += 1
+
+    idx = n
+    
+    # Variable to keep track of the count of papers
+    # having at least idx citations
+    s = freq[n]
+    while s < idx:
+        idx -= 1
+        s += freq[idx]
+    
+    # Return the largest index for which the count of 
+    # papers with at least idx citations becomes >= idx
+    return idx
+
+if __name__ == '__main__':
+    citations = [6, 0, 3, 5, 3]
+    print(hIndex(citations))
